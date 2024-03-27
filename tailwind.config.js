ff0747d8ff7906a933d9516ebe832d046e7cbd4c
junayet <muhammadjunayetmaruf@gmail.com>
2024-03-27 15:41:33 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#fe3938',
+        secondary: '#363383',
+      }
+    },
+  },
+  plugins: [require("daisyui")],
+}
+
